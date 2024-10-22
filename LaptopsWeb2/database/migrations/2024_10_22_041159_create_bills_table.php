76a6093358a8e1c6bf15e7fa0474e613e6431d31
TranNgocHien253 <161793880+TranNgocHien253@users.noreply.github.com>
2024-10-22 11:42:52 +0700

    comit bố cục

--- LaptopsWeb2/database/migrations/2024_10_22_041159_create_bills_table.php
+++ LaptopsWeb2/database/migrations/2024_10_22_041159_create_bills_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('bills', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('id_customer')->constrained('customers');
+            $table->date('date_order');
+            $table->float('total');
+            $table->string('payment', 50);
+            $table->string('note', 255)->nullable();
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('bills');
     }
 };
