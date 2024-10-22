e9f3198397e565ce8f62cad86f1788222b21d972
TranNgocHien253 <161793880+TranNgocHien253@users.noreply.github.com>
2024-10-22 21:31:28 +0700

    comit slides

--- LaptopsWeb2/resources/views/app.blade.php
+++ LaptopsWeb2/resources/views/app.blade.php
@@ -1,13 +1,16 @@
 <!doctype html>
 <html>
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite('resources/css/app.css')
 </head>
+
 <body>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div class="container mx-auto px-4 mt-5">
+    @yield('content')
+  </div>
 </body>
+
 </html>
--- LaptopsWeb2/routes/web.php
+++ LaptopsWeb2/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SlideController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('app');
 });
+
+
+
+
+// Routes for slides
+Route::get('/slides/create', [SlideController::class, 'create'])->name('admin.slides.create');
+Route::post('/slides', [SlideController::class, 'store'])->name('admin.slides.store');
+
+Route::get('/slides', [SlideController::class, 'index'])->name('admin.slides.index');
+
+Route::get('/slides/{id}/edit', [SlideController::class, 'edit'])->name('admin.slides.edit');
+Route::put('/slides/{id}', [SlideController::class, 'update'])->name('admin.slides.update');
+
+
+Route::delete('/slides/{id}', [SlideController::class, 'destroy'])->name('admin.slides.destroy');
