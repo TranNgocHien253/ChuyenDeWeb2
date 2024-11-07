b0fc4b735d756c70dd46ce6e5418138d3f31291b
TranNgocHien253 <161793880+TranNgocHien253@users.noreply.github.com>
2024-11-07 09:44:17 +0700

    n

--- LaptopsWeb2/resources/views/admin/user/index.blade.php
+++ LaptopsWeb2/resources/views/admin/user/index.blade.php
@@ -84,7 +84,7 @@
                 <li><a href="{{ $profiles->previousPageUrl() }}" class="flex gap-1 items-center justify-center px-3 h-8 leading-tight text-gray-500 hover:text-gray-700">
                         <svg class="w-2.5 h-2.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
-                        </svg>Pre< /a>
+                        </svg>Pre </a>
                 </li>
                 @endif
 
