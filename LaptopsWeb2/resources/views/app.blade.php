c2846458617e4d33453af17f95d0a7f8b3abae48
zusmorke <dinhlove2k4@gmail.com>
2024-11-19 10:04:00 +0700

    debug lôi xoa giao dien

--- LaptopsWeb2/resources/views/app.blade.php
+++ LaptopsWeb2/resources/views/app.blade.php
@@ -6,6 +6,7 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
   <title>LAPTOPs | Web 2</title>
+  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
   @vite('resources/css/app.css')
