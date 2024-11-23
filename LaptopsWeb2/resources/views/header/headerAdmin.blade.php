d2fc348b810b7a0108bcaa1a2414ff155181f209
ngocyen12abc <tranngocyen12122003@gmail.com>
2024-11-23 10:35:20 +0700

    wishlist

--- LaptopsWeb2/resources/views/header/headerAdmin.blade.php
+++ LaptopsWeb2/resources/views/header/headerAdmin.blade.php
@@ -502,21 +502,14 @@
                                     d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                             </svg>
                         </span>
-<<<<<<< HEAD
                       
-=======
->>>>>>> 8e688aa4c8b45af852d721fec63aa3227703b97a
                         <form class="flex items-center max-w-sm w-full" action="{{ route('search') }}" method="GET">
                 <label for="simple-search" class="sr-only">Search</label>
                 <div class="relative w-full">
                     <input type="text" name="search" id="simple-search"
-<<<<<<< HEAD
                            class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg shadow-md focus:ring-2 focus:ring-purple-600 focus:border-transparent block w-full pl-3 p-2.5"
                            placeholder="Search..." required />
-=======
-                        class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg shadow-md focus:ring-2 focus:ring-purple-600 focus:border-transparent block w-full pl-3 p-2.5"
-                        placeholder="Search..." required />
->>>>>>> 8e688aa4c8b45af852d721fec63aa3227703b97a
+                   
                     <button type="submit" class="absolute inset-y-0 right-0 flex items-center px-3">
                         <svg class="w-4 h-4 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
@@ -525,10 +518,7 @@
                     </button>
                 </div>
             </form>
-<<<<<<< HEAD
 
-=======
->>>>>>> 8e688aa4c8b45af852d721fec63aa3227703b97a
                     </div>
 
                     <!-- Panel content (Search result) -->
